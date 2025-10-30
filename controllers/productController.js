fa4880653d50d80e2b6493b722e8408ead6cbc3b
Uveis DZ <uveis2005@gmail.com>
2025-10-30 11:54:54 +0300

    Add files via upload

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,6 +16,46 @@ class ProductController {
         }
     }
 
+    async getProducts(req, res) {
+        try {
+            const products = await Product.find()
+            
+            return res.json(products);
+        } catch (error) {
+            return res.status(500).json({ error: error.message })
+        }
+    }
+
+    async getOneProduct(req, res) {
+        try {
+            const { id } = req.params
+            const product = await Product.findById(id);
+
+            return res.json(product);
+        } catch (error) {
+            console.log("Ошибка при получении товара \n", error)
+            return res.status(500).json({ error: error.message })
+        }
+    }
+
+    async updateProduct(req, res) {
+        try {
+            const product = req.body
+            console.log(product._id)
+
+            if(!product._id) {
+                res.status(400).json({ message: "ID не указан" });
+            }
+
+            const updateProduct = await Product.findByIdAndUpdate(product._id, product, { new: true });
+
+            console.log("Товар успешно обновлен");
+            return res.json(updateProduct);
+        } catch (error) {
+            return res.status(500).json({ error: error.message })
+        }
+    }
+
 }
 
 
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,10 +1,16 @@
 const Router = require('express');
 const ProductController = require('../controllers/productController.js');
+const productController = require('../controllers/productController.js');
 
 const router = new Router();
 
 
-router.post('./product', ProductController.addProduct);
+router.post('/product', ProductController.addProduct);
+router.get("/products", ProductController.getProducts);
+router.get("/product/:id", ProductController.getOneProduct);
+router.put("/product/update", productController.updateProduct);
+// router.patch("/updateproduct");
+// router.delete("/product/:id");
 
 
 module.exports = router
